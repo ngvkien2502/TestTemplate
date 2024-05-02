bab0233f0c261c7ed10b3b6169703e43a52535d1
ngvkien2502 <ngvkien2502@gmail.com>
2024-05-02 23:46:47 +0700

    feature: template custom person picture

---
+++ ChatProjectAssign/Resources/Templates/KPersonPicture.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace ChatProjectAssign.Resources.Templates;
+public class KPersonPicture : Control
+{
+    public string NoteText
+    {
+        get => (string)GetValue(NoteTextProperty);
+        set => SetValue(NoteTextProperty, value);
+    }
+    public static readonly DependencyProperty NoteTextProperty =
+        DependencyProperty.Register(nameof(NoteText), typeof(string), typeof(KPersonPicture), new PropertyMetadata(null));
+}
